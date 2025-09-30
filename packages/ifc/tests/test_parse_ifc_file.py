9e9bf0e3c9b3d71173f06d28d16f6c00f2b1207d
Alex Morris <alex@navra.nz>
2025-10-01 11:23:22 +1300

    Initial IFC package

---
+++ packages/ifc/tests/test_parse_ifc_file.py
@@ -0,0 +1,18 @@
+#  Copyright © 2025 Bentley Systems, Incorporated
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+class TestParseIfcFile:
+    """Test the parse_ifc_file function behaves as intended."""
+
+    def test_parse_valid_ifc_file(self):
+        """Test parsing a valid IFC file."""
+        assert True is True  # TODO: Replace with actual test code
